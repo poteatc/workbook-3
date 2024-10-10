c592665dad07c7a9fbef82faa8ad49b3962fae67
poteatc <poteatc@outlook.com>
2024-10-10 09:34:56 -0400

    Added FormatDates application that prints the LocalDate in several different formats using DateTimeFormatter

--- FormatDates/src/main/java/com/pluralsight/dates/FormatDates.java
+++ FormatDates/src/main/java/com/pluralsight/dates/FormatDates.java
@@ -1,4 +1,43 @@
 package com.pluralsight.dates;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.TimeZone;
+import java.text.DateFormat;
+
 public class FormatDates {
+    public static void main(String[] args) {
+        String pattern = "MM/dd/yyyy";
+        // Create class to format date
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern(pattern);
+
+        LocalDate ld = LocalDate.now();
+        System.out.println("Formatting date: " + ld);
+        System.out.println(fmt.format(ld));
+        fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        System.out.println(fmt.format(ld));
+        fmt = DateTimeFormatter.ofPattern("MMMM dd, yyyy");
+        System.out.println(fmt.format(ld));
+
+        // print date and GMT time
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+        DateFormat dateTime = new SimpleDateFormat("HH:mm");
+        // Change time zone to GMT
+        dateTime.setTimeZone(TimeZone.getTimeZone("GMT"));
+        String currentTime = dateTime.format(calendar.getTime());
+        fmt = DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy " + currentTime);
+        System.out.println(fmt.format(ld));
+
+        // Change time format
+        dateTime = new SimpleDateFormat("H:mm");
+        calendar = Calendar.getInstance(TimeZone.getTimeZone("EST"));
+        dateTime.format(calendar.getTime());
+        currentTime = dateTime.format(calendar.getTime());
+        // 5:02 on 05-Sep-2021
+        DateTimeFormatter timeFmt = DateTimeFormatter.ofPattern(currentTime);
+        fmt = DateTimeFormatter.ofPattern("dd-MMM-yyyy");
+        System.out.print(timeFmt.format(ld) + " on " + fmt.format(ld));
+    }
 }
